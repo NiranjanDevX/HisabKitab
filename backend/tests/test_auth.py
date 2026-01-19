970e5c87f9d7e039fb122e113ecded4c59491f9b
niranjan87 <2100090187csit@gmail.com>
2026-01-19 12:10:07 +0545

    Added test files

---
+++ backend/tests/test_auth.py
@@ -0,0 +1,25 @@
+"""
+Authentication Tests
+"""
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_user_registration():
+    """Test user registration"""
+    # TODO: Implement test
+    pass
+
+
+@pytest.mark.asyncio
+async def test_user_login():
+    """Test user login"""
+    # TODO: Implement test
+    pass
+
+
+@pytest.mark.asyncio
+async def test_token_refresh():
+    """Test token refresh"""
+    # TODO: Implement test
+    pass
